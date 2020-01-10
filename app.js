32b3c09e92c42fb911216718fcb1f33aeddea076
Christopher Floyd <cfloyd252@gmail.com>
2020-01-10 13:14:52 -0700

    modified app.js and added an html file

--- app.js
+++ app.js
@@ -7,24 +7,54 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What is the name of the robot that follows your character  around?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'FlapJack',
+        'EDI',
+        'Claptrap',
+        'E2-DR'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'Claptrap'
     },
     {
-      question: 'What is the current year?',
+      question: 'Where can you get a double penetrating unkempt harold weapon ?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Sanctuary',
+        'Claptraps place',
+        'Southern shelf',
+        'Three horns divide'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Three horns divide'
+    }
+    {
+      question: 'What is the name of the man who helps put the robots eye back into place?',
+      answers: [
+        'Sir mix a lot',
+        'Siracha',
+        'Sir Hammerlock',
+        'Sir nighteye death'
+      ],
+      correctAnswer: 'Sir Hammerlock'
+    }
+    {
+      question: 'Where do you farm the easy infinity pistol?',
+      answers: [
+        'Three horns drive',
+        'Sanctuary',
+        'Claptraps place',
+        'Southern Shelf'
+      ],
+      correctAnswer: 'Three horns drive'
+    }
+    {
+      question: 'What is the name of the first boss you get to kill in Borderlands 2?',
+      answers: [
+        'Knuckledragger',
+        'Badassarus',
+        'Dukinomom',
+        'Terramorphus the invincible'
+      ],
+      correctAnswer: 'Three horns drive'
     }
   ],
   questionNumber: 0,
@@ -46,9 +76,10 @@ const store = {
 
 function render(){
   //render the next
+  console.log('render is working');
 }
 function startQuiz(){
-  //when button is clicked, the first question page will render 
+  //when button is clicked, the first question view will render 
 }
 function submitAnswer(){
   //when button is clicked, the results page will render with the correct answer 
@@ -58,7 +89,7 @@ function nextQuestion(){
   //when button is clicked, the next question page will render 
 }
 function restartQuiz(){
-  //when button is clicked, start page will render
+  //when button is clicked, start view will render 
 }
 
 function main(){
